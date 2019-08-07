6d09a92f9960f228efd36ca9b6297a17505a9687
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2019-08-07 23:42:24 +0100

    Finish styling of user details form

--- src/components/FormUserDetails.js
+++ src/components/FormUserDetails.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
 import TextField from 'material-ui/TextField';
-import moduleName from 'material-ui/RaisedButton';
+import RaisedButton from 'material-ui/RaisedButton';
 
 export class FormUserDetails extends Component {
 	continue = event => {
@@ -17,14 +17,48 @@ export class FormUserDetails extends Component {
 				<React.Fragment>
 					<AppBar title="Enter User Details" />
 					<TextField
-					hintText="Enter your first and last name"
+					hintText="Ex. Jacob Trayson"
 					floatingLabelText="Name"
 					onChange={handleChange('name')} // Destructure. No need for this.props.handleChange
 					defaultValue={values.name}
 					/>
+					<br />
+					<TextField
+					hintText="Ex. Web Developer"
+					floatingLabelText="Role"
+					onChange={handleChange('role')} // Destructure. No need for this.props.handleChange
+					defaultValue={values.role}
+					/>
+					<br />
+					<TextField
+					hintText="Ex. jacob@trayson.com"
+					floatingLabelText="Email"
+					onChange={handleChange('email')} // Destructure. No need for this.props.handleChange
+					defaultValue={values.email}
+					/>
+					<br />
+					<TextField
+					hintText="•••••••••"
+					floatingLabelText="Password"
+					onChange={handleChange('password')} // Destructure. No need for this.props.handleChange
+					defaultValue={values.password}
+					/>
+					<br />
+					<RaisedButton
+					label="submit"
+					primary={true}
+					style={styles.button}
+					onClick={this.continue}
+					/>
 				</React.Fragment>
 			</MuiThemeProvider>
 		);
+	}
+}
+
+const styles = {
+	button: {
+		margin: 15
 	}
 }
 
